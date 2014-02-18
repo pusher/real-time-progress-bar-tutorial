9dc18af52ac298e207722f20bfd54093af735bae
Syl <sgiuliani@gmail.com>
2014-02-18 16:19:43 +0000

    revert to using Pusher.js from the CDN

--- blog_post.md
+++ blog_post.md
@@ -1,15 +1,15 @@
-When I browse the web, there's nothing less helpful than a spinning wheel or a progress bar that doesn't give me any feedback on what is actually happening. It usually happen when the application is working on a task that needs some time to process like data or file import, provisioning an account or a big search query. 
+When I browse the web, there's nothing less helpful than a progress bar that doesn't give me any feedback on what is actually happening. It usually happen when the application is working on a task that needs some time to process like data/file import, provisioning an account or a big search query.
 
-This usually leave me, the poor user, wondering if something wrong with the app? Should I wait 5 more minutes before I refresh the page? As a user, this is such a frustrating experience!
+This usually leave me, the poor user, wondering if something went wrong with the app? Should I wait 5 more minutes before I refresh the page? As a user, this is such a frustrating experience!
 
-To cope with this problem and improve the user experience in your app, you should use a real-time progress bar. Lucky for you, Pusher make it dead easy for your to build one!
+To cope with this problem and improve the user experience in your app, you should use a **real-time progress bar**. Lucky for you, [Pusher](http://www.pusher.com) make it dead easy for your to build one!
 
 
 ## The problem
 
-For this demo, we will use the all too common problem of when someone create and account and the app needs to provision the account with a demo environment for example.
+For this demo, we will use the all too common problem of creating an account and the app needs to provision the account with some kind of data.
 
-<Screenshot of create button and spinning wheel>
+<Screenshot of end button and spinning wheel>
 
 
 ## As easy as 1, 2, 3
--- public/js/home.js
+++ public/js/home.js
@@ -1,43 +1,43 @@
 $(document).ready(function(){
-	// Fake Random User Unique ID, I usually use Mixpanel or Kissmetrics UID here will a fallback function for people who block their JS libraries
-	var uID = Math.floor((Math.random()*100)+1)
+  // Fake Random User Unique ID, I usually use Mixpanel or Kissmetrics UID here will a fallback function for people who block their JS libraries
+  var uID = Math.floor((Math.random()*100)+1)
 
 
-	// Instantiate Pusher
-	var pusher = new Pusher('0b4b9a3e205f2d9f7766') // Change it to your Pusher APP Key
+  // Instantiate Pusher
+  var pusher = new Pusher('0b4b9a3e205f2d9f7766') // Change it to your Pusher APP Key
   var channel = pusher.subscribe('signup_process_'+uID) // The Channel you want to subscribe to
 
   channel.bind('update', function(data) { // Bind to an event on our channel, in our case, update
-  	var messageBox = $('#create-account-form-with-realtime').children('.messages')
-  	var progressBar = $('#realtime-progress-bar')
+    var messageBox = $('#create-account-form-with-realtime').children('.messages')
+    var progressBar = $('#realtime-progress-bar')
 
-  	progressBar.width(data.progress+"%")
+    progressBar.width(data.progress+"%")
     messageBox.html(data.message)
 
     // Process is complete,Do whatever you want now, maybe redirect them to their freshly created account?
     if (data.progress == 100) {
-    	messageBox.html('Was it better with this process?')
+      messageBox.html('Was it better with this process?')
     }
   });
 
   // Submit the forms using AJAX, nothing to see here.
-	$('form').submit(function (e) {
-		e.preventDefault();
-		var name = $('#name').val() || "Bob"
-		var form = this
-		var btn = $(form).find('button')
-		var progressBar = $(form).find('.progress')
-		progressBar.removeClass('hide')
-		btn.prop('disabled', true)
+  $('form').submit(function (e) {
+    e.preventDefault();
+    var name = $('#name').val() || "Bob"
+    var form = this
+    var btn = $(form).find('button')
+    var progressBar = $(form).find('.progress')
+    progressBar.removeClass('hide')
+    btn.prop('disabled', true)
 
-		$.post( $(form).attr('action'),{name: name, uid: uID} , function () {
-		}).done(function(response) {
-			btn.prop('disabled', false)
+    $.post( $(form).attr('action'),{name: name, uid: uID} , function () {
+    }).done(function(response) {
+      btn.prop('disabled', false)
 
-			progressBar.toggleClass('active')
-			if (!$(form).attr('id')) {
-				$(form).children('.messages').html(response)
-			}
-		})
-	})
+      progressBar.toggleClass('active')
+      if (!$(form).attr('id')) {
+        $(form).children('.messages').html(response)
+      }
+    })
+  })
 })
